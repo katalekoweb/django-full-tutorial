8ea2ce20c215ac83b2d4552acb4f585ece06ecd3
Kataleko <katalekoweb@gmail.com>
2025-07-27 10:41:54 +0100

    feat: post details. route params

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "posts"
+
 urlpatterns = [
-    path("", views.post_list, name="post_list"),
+    path("", views.posts, name="list"),
+    path("<slug:slug>", views.post, name="page")
 ]
--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
-def post_list(request):
-    return render(request, 'posts/post_list.html', {})
+def posts(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'posts/post_list.html', {'posts': posts})
+
+def post (request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post.html', {'post': post})
